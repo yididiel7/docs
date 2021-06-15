093eb21e682ae02442f66d0dc91c65d45505cd8f
gangwerz <gangwerz@users.noreply.github.com>
2021-06-15 21:41:18 +0000

    deploy: 2fd52b67487d19784fc11d338a7747a213a1a1d7

--- _nuxt/static/1623775364/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623775364",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d725eb41"}},_img:{"/_ipx/images/website/robot-type.png?s=1207_440":"\u002F_nuxt\u002Fimage\u002Fe01611.png"}}}(null,"\u002F"));
---
+++ _nuxt/static/1623793233/articles/dash-to-dock/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623793233",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fdash-to-dock",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f2f5ec12"}}}}(null));
