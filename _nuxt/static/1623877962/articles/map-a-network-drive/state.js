7e695eb3bad86e3cd4c9c2226a2cc0a062843368
storyaddict <storyaddict@users.noreply.github.com>
2021-06-17 23:34:21 +0000

    deploy: c86f3e581c2636b172dc65e5a70576a9af6d5256

--- _nuxt/static/1623877962/articles/map-a-network-drive/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623877962",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fmap-a-network-drive",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"abbe1c35"}}}}(null));
---
+++ _nuxt/static/1623972818/articles/show-battery-percentage/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623972818",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fshow-battery-percentage",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f7b4a41"}}}}(null));
