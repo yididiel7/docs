8ef69026a532bbd17ee79acf281a302339d07628
storyaddict <storyaddict@users.noreply.github.com>
2021-06-18 18:08:51 +0000

    deploy: e8841f634b5f8ca8582774c547daf15ca5c53447

---
+++ _nuxt/static/1624039683/articles/upgrade-ubuntu/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624039683",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fupgrade-ubuntu",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b80de0b9"}}}}(null));
