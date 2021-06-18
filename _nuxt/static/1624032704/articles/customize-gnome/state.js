feb607a3732e62209711c6566025a365a4b03ba8
storyaddict <storyaddict@users.noreply.github.com>
2021-06-18 16:22:01 +0000

    deploy: fc4daa7758585bea6d87cb6d99860044737375da

--- _nuxt/static/1624032704/articles/customize-gnome/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624032704",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fcustomize-gnome",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c91d506e"}}}}(null));
