2264972dee7f1024892a2e9f1bd815add2847684
storyaddict <storyaddict@users.noreply.github.com>
2021-06-18 19:02:29 +0000

    deploy: 5087b1c7855b9c2403c7f04df25fc376fe40039b

--- _nuxt/static/1624039683/articles/system76-software/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624039683",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fsystem76-software",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b80de0b9"}}}}(null));
