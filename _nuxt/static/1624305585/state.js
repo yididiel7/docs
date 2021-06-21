a53a3210dda1f1266525930ec094cd2a86e1603d
ahoneybun <ahoneybun@users.noreply.github.com>
2021-06-21 20:57:59 +0000

    deploy: 6b9090aca8f116aad4fe4a3f93c5464928502dfb

--- _nuxt/static/1624305585/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624305585",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"29ac220b"}},_img:{"/_ipx/images/website/robot-type.png?s=1207_440":"\u002F_nuxt\u002Fimage\u002Fe01611.png"}}}(null,"\u002F"));
