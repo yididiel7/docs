bd397e5fbf91a7fd606de7f48c766573559b8e04
btkostner <btkostner@users.noreply.github.com>
2021-06-21 16:50:22 +0000

    deploy: ee3eb8b0012cbea014df120234b7dd3ca40923ba

---
+++ _nuxt/static/1624294142/articles/dash-to-dock/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624294142",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fdash-to-dock",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"277e40a6"}}}}(null));
