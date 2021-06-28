5526bcc04414ce837d7d0f164729b58aa7813725
jacobgkau <jacobgkau@users.noreply.github.com>
2021-06-28 14:41:30 +0000

    deploy: 6131a73242e592cb8e8595e9e0341221a3ae08b5

--- _nuxt/static/1624743786/articles/deep-learning-steve-hart-university-of-colorado/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624743786",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fdeep-learning-steve-hart-university-of-colorado",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a9d5a61"}}}}(null));
