3d1196d61a6130543102e5ff84e77671a7fbf835
storyaddict <storyaddict@users.noreply.github.com>
2021-06-30 17:23:56 +0000

    deploy: bed01b5ac804ee1ecc508bbabd87848d105c0fdd

--- _nuxt/static/1625071272/articles/open-firmware-systems/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625071272",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fopen-firmware-systems",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"16659e1e"}}}}(null));
