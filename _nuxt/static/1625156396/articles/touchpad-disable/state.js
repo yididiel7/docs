181fe0d1ee647b71f453a23fa732acecfb2cf198
storyaddict <storyaddict@users.noreply.github.com>
2021-07-07 15:33:42 +0000

    deploy: 167289995dc226121e6fba2023f4a3439b2ffcac

--- _nuxt/static/1625156396/articles/touchpad-disable/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625156396",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Ftouchpad-disable",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6e0e4988"}}}}(null));
