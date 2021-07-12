8763a0177bcb9a002ba44f2495d820abacd60b0f
btkostner <btkostner@users.noreply.github.com>
2021-07-12 19:02:33 +0000

    deploy: 7289a066574531f4f15ee794f85699cfd0c4eedd

--- _nuxt/static/1626108272/articles/web-design-david-trounce-mallee/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626108272",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fweb-design-david-trounce-mallee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3325c542"}}}}(null));
