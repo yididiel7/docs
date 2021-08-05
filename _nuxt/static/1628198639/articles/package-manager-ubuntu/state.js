f711de3cc1c62aef7993ee2160af0250c5627b62
jacobgkau <jacobgkau@users.noreply.github.com>
2021-08-05 21:34:44 +0000

    deploy: 77545d6444516365247b9b555c02975f6bad0b04

---
+++ _nuxt/static/1628198639/articles/package-manager-ubuntu/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628198639",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fpackage-manager-ubuntu",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"be26006c"}}}}(null));
