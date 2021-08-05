41a96ec181d0bf8966c3ded7f5a8700bb0a59a5a
jacobgkau <jacobgkau@users.noreply.github.com>
2021-08-05 21:24:40 +0000

    deploy: a4f50594bfcd7384b2df18cd9c66d4eb687fe10f

---
+++ _nuxt/static/1628198105/articles/bluetooth/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628198105",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fbluetooth",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"175bad00"}}}}(null));
