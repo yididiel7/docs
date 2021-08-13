130c0a94a04f77397b349483c6da691241956b9b
jacobgkau <jacobgkau@users.noreply.github.com>
2021-08-13 19:05:49 +0000

    deploy: 485e2e4af4cffb87363a20b0a167d9835079975a

---
+++ _nuxt/static/1628881025/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628881025",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b80edd5d"}},_img:{"/_ipx/images/website/robot-type.png?s=1207_440":"\u002F_nuxt\u002Fimage\u002Fe01611.png"}}}(null,"\u002F"));
